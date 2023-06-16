6b5b7259797b403856ab21a3a6ccdbf3c31a8e31
Mononito Goswami <mononitog@hotmail.com>
2023-06-16 10:09:24 -0700

    Update loader.py

--- src/tsadams/loaders/loader.py
+++ src/tsadams/loaders/loader.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
 import torch as t
