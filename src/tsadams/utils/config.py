5bec96781d70004aae07b774edfb17e1544ed195
Mononito Goswami <mononitog@hotmail.com>
2023-06-16 10:07:48 -0700

    Update config.py

--- src/tsadams/utils/config.py
+++ src/tsadams/utils/config.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from yaml import load, dump
 from yaml import CLoader as Loader, CDumper as Dumper
 
